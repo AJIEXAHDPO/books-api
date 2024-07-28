10dc630bfca332163216ba873aaf0518b63954f3
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-07-28 13:32:02 +0300

    add controller inner logic

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Request\AuthorCreateRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AuthorController extends AbstractController
 {
-    #[Route('/author', name: 'app_author')]
+    #[Route('/author', name: 'app_author', methods: ['get'])]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/AuthorController.php',
+        ]);
+    }
+
+    #[Route('/author', name: 'create_author', methods: ['post'])]
+    public function create(AuthorCreateRequest $request): JsonResponse
+    {
+        //$request->validate();
+
+        return $this->json([
+            'Created author'
         ]);
     }
 }
--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,18 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Request\BookCreateRequest;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BookController extends AbstractController
 {
-    #[Route('/book', name: 'app_book')]
-    public function index(): JsonResponse
+    #[Route('/book', name: 'app_book', methods: ['get'])]
+    public function index(ManagerRegistry $doctrine): JsonResponse
     {
+        $books = $doctrine->getRepository(Book::class)->findAll();
+        $booksAll = [];
+        foreach ($books as $book) {
+            array_push($booksAll, $book->getAll());
+        }
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
+            'books' => $booksAll,
         ]);
+    }
+
+    #[Route('/book', name: 'create_book', methods: ['post'])]
+    public function create(ManagerRegistry $doctrine, #[MapRequestPayload] BookCreateRequest $request): JsonResponse
+    {
+        
+        return $this->json([
+            "Created!!!"
+        ], status: 201);
+    }
+
+    #[Route('/book/{id}', name: 'delete_book', methods: ['delete'])]
+    public function remove(ManagerRegistry $doctrine, int $id): JsonResponse
+    {
+        $entityManager = $doctrine->getManager();
+        $book = $doctrine->getRepository(Book::class)->find($id);
+
+        if (!$book) {
+            return $this->json(["Not found"], status: 404);
+        }
+
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->json([
+            "deleted successfully"
+        ], status: 204);
     }
 }
